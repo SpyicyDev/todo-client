6ebfd4cfe3ef3b931531cb5222cb4f629ce52e21
Mack Haymond <mack.haymond@icloud.com>
2023-03-15 19:56:13 -0400

    ok I think its fixed now

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 curl https://sh.rustup.rs -sSf | sh -s -- -y
+
 export PATH="$HOME/.cargo/env:$PATH"
+source "$HOME/.cargo/env"
+
 $HOME/.cargo/bin/cargo install --locked trunk
 $HOME/.cargo/bin/trunk build
