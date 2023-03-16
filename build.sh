c759d4349ab4f02b3b0d5d6fb8d43914db1c1fdc
Mack Haymond <mack.haymond@icloud.com>
2023-03-15 20:30:15 -0400

    ok I think its fixed now

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH="$HOME/.cargo/env:$PATH"
 source "$HOME/.cargo/env"
 
+rustup toolchain install nightly
+rustup default nightly
+rustup target add wasm32-unknown-unknown
+
 $HOME/.cargo/bin/cargo install --locked trunk
 $HOME/.cargo/bin/trunk build
