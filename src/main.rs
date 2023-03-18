e7d4dc447c53619f16b4c9a511a21df3e864daa7
Mack Haymond <mack.haymond@icloud.com>
2023-03-18 11:37:27 -0400

    let's make fetch work now

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,11 @@
-mod sql;
 mod components;
+mod server;
 
 use leptos::*;
 use crate::components::*;
-
-// use crate::sql::sql;
-
-use futures::executor::*;
 
 fn main() {
     mount_to_body(|cx| view! {cx,
         <TodoList/>
     });
-
-    /*
-    let res = sql(0, "", 2).unwrap().unwrap();
-    res.iter().for_each(
-        |todo| println!("id: {}, task: {}", todo.todo_id, todo.todo)
-    );
-     */
 }
